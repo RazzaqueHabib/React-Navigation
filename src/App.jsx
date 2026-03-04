34c4ccbde80c427c112572231a5c62addf75d3e2
Abdur Razzaque <razzakhabib20@gmail.com>
2026-03-04 19:51:52 +0600

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import DaisyNav from "./components/DaisyNav/DaisyNav"
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+  <DaisyNav></DaisyNav>
+  <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
